8a8d0c6fad5d4f4060600b115e8de5158b0924f2
Sotiraq Saraci <saraci.sotiraq@gmail.com>
2023-10-21 11:30:57 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EvidentaMasini.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
